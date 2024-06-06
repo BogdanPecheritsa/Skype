59a5ee6761cb2556083bfc36a9b84e55712968cf
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-06-06 12:28:00 +0300

    Created Controller and View for each model class, modified _Layout.cshtml(added Controllers in navmenu), modified Index.cshtml of each View(change text Index to Controller name)

---
+++ Controllers/CallsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CALLОМОЙСЬКИЙ.Data;
+using CALLОМОЙСЬКИЙ.Models.Domain;
+
+namespace CALLОМОЙСЬКИЙ.Controllers
+{
+    public class CallsController : Controller
+    {
+        private readonly ProjectContext _context;
+
+        public CallsController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Calls
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Calls.ToListAsync());
+        }
+
+        // GET: Calls/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var call = await _context.Calls
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (call == null)
+            {
+                return NotFound();
+            }
+
+            return View(call);
+        }
+
+        // GET: Calls/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Calls/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CallerUserId,ReceiverUserId,StartTime,EndTime")] Call call)
+        {
+            if (ModelState.IsValid)
+            {
+                call.Id = Guid.NewGuid();
+                _context.Add(call);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(call);
+        }
+
+        // GET: Calls/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var call = await _context.Calls.FindAsync(id);
+            if (call == null)
+            {
+                return NotFound();
+            }
+            return View(call);
+        }
+
+        // POST: Calls/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,CallerUserId,ReceiverUserId,StartTime,EndTime")] Call call)
+        {
+            if (id != call.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(call);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CallExists(call.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(call);
+        }
+
+        // GET: Calls/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var call = await _context.Calls
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (call == null)
+            {
+                return NotFound();
+            }
+
+            return View(call);
+        }
+
+        // POST: Calls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var call = await _context.Calls.FindAsync(id);
+            if (call != null)
+            {
+                _context.Calls.Remove(call);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CallExists(Guid id)
+        {
+            return _context.Calls.Any(e => e.Id == id);
+        }
+    }
+}
